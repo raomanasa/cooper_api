a7eda8ca8d6f523b8d3f1529d49b4725bdfd56f0
Manasa Rao <drmanasa18.rao@gmail.com>
2020-01-08 15:50:10 +0100

    adds hstore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
-
-
 class User < ActiveRecord::Base
   extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :performance_data, class_name: 'PerformanceData'
 end
